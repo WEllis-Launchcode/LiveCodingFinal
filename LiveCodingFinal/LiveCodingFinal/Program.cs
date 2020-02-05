c5ce91a6a6fd8e0a2cf196de3e14ff7b6d3b65de
WEllis-Launchcode <54691108+WEllis-Launchcode@users.noreply.github.com>
2020-02-04 19:44:41 -0500

    Add files via upload

---
+++ LiveCodingFinal/LiveCodingFinal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LiveCodingFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weather;
+            int tempature;
+            Console.WriteLine("Enter the weather: ");
+            weather = Console.ReadLine();
+            Console.WriteLine("Enter the temature: ");
+            tempature = Convert.ToInt32(Console.ReadLine());
+            if ((weather == "clear") && (tempature == 50))
+            {
+                double total = (250 + (250 * 0.10) + (250 * 0)) *3;
+                Console.WriteLine(total);
+            }
+
+            if ((weather == "clear") && (tempature >= 50 && tempature <= 59))
+            {
+                double total = (250 + (250 * 0.10) + (250 * 0)) * 3;
+                Console.WriteLine(total);
+            }
+            if ((weather == "full moon") && (tempature >= 40 && tempature <= 49))
+            {
+                double total = (250 + (250 * 0.25) - (250 * 0.05)) * 3;
+                Console.WriteLine(total);
+            }
+            if ((weather == "raining") && (tempature >= 70 && tempature <= 79))
+            {
+                double total = (250 - (250 * 0.25) + (250 * 0.20)) * 3;
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
